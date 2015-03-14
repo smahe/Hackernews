8aa7847814a9576eb89cf4446350d9d90bd548dd
SMahendren <smahendren37@gmail.com>
2015-03-14 21:35:12 +0530

    Saving to mongodb

--- scrape.js
+++ scrape.js
@@ -3,6 +3,20 @@
 var request = require('request');
 var express = require('express');
 var cheerio = require('cheerio');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/hn',function(err){
+					if(err){
+						console.log("error mongodb connection");
+					}else
+					console.log("success");
+				});
+var hnSchema = mongoose.Schema({title : String,rank : Number,url : String,username : String,points : Number
+								});
+
+//model param1 Collection_name,param2 schemaname
+var hnModel = mongoose.model('Entry',hnSchema);
+
 //cheerio a jquery library for node
 	request('https://news.ycombinator.com',function(error,response,htmlbody){
 		var parsedResults=[];
@@ -18,18 +32,36 @@ var cheerio = require('cheerio');
 						var pts = $(subtext).eq(0).text();
 						var username = $(subtext).eq(1).text();
 						//var comments = $(subtext).eq(2).text();
-						console.log(rank);
+//						console.log(rank);
 						//single json object containg data
-						var metadata = {
-							rank:parseInt(rank),
-							title:title,
-							url:url,
-							username:username,
-							points:parseInt(pts)
-						};
+						// var metadata = {
+						// 	title:title,
+						// 	rank:parseInt(rank),
+						// 	url:url,
+						// 	username:username,
+						// 	points:parseInt(pts)
+						// };
+						var scrapedData = new hnModel({
+							title : title,
+							rank : parseInt(rank),
+							url : url,
+							username : username,
+							points : parseInt(pts)
+						});
+						scrapedData.save(function(err){
+							if(err){
+								throw err;
+							}
+							//console.log("Error in saving");
+						//throw err;
+					});
+
 						//json array
-						parsedResults.push(metadata);
-				});
+						//parsedResults.push(metadata);
+				//});
+					
+			});
+
 				//Todo create mongo db to store data
 				//create a cron job to automatically run task at needed time
 				//console.log(parsedResults);
